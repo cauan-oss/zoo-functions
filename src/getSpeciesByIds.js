889dd01d5b8bd98abcfcefbf2a5a41c5603d8e40
Cauan <cauanbarros14@gmail.com>
2022-09-29 19:11:07 -0300

    requisito 1 concluido

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,10 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  
-}
+function getSpeciesByIds(...ids) {
+  const meuArray = [];
 
+  if (ids.length === 0) {
+    return meuArray;
+  } return data.species.filter((specie) => ids.includes(specie.id));
+}
 module.exports = getSpeciesByIds;
