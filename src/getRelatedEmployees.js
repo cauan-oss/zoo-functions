c630cab4ddd4e100f80645d0d14a7c119c7d8b54
Cauan <cauanbarros14@gmail.com>
2022-09-30 10:40:48 -0300

    requisito 4 concluido

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
-}
+  return data.employees.some((employee) => employee.managers.includes(id));
+};
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (!isManager(managerId)) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  if (!managerId) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const myArray = [];
+  data.employees.forEach((employee) => {
+  if (employee.managers.includes(managerId)) myArray.push(`${employee.firstName} ${employee.lastName}`);
+  });
+  return myArray;
 }
 
+console.log(getRelatedEmployees('9e7d4524-363c-416a-8759-8aa7e50c0992'))
 module.exports = { isManager, getRelatedEmployees };
