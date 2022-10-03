5ea4b91544c956786c1d5c0a37915b0deb71b9be
Cauan <cauanbarros14@gmail.com>
2022-10-03 11:57:06 -0300

    atualizando

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,18 +1,21 @@
-const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function isManager(id) {
   return data.employees.some((employee) => employee.managers.includes(id));
-};
+}
 
 function getRelatedEmployees(managerId) {
-  if (!isManager(managerId)) throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  if (!isManager(managerId)) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
   if (!managerId) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
   }
   const myArray = [];
   data.employees.forEach((employee) => {
-  if (employee.managers.includes(managerId)) myArray.push(`${employee.firstName} ${employee.lastName}`);
+    if (employee.managers.includes(managerId)) {
+      myArray.push(`${employee.firstName} ${employee.lastName}`);
+    }
   });
   return myArray;
 }
