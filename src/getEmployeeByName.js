e73ba91ff357747ae9bbd8ce39887679d98c8161
Cauan <cauanbarros14@gmail.com>
2022-10-03 11:47:18 -0300

    arrumando

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -2,7 +2,8 @@ const data = require('../data/zoo_data');
 
 function getEmployeeByName(...employeeName) {
   if (employeeName.length === 0) return {};
-  return data.employees.find((manager) => employeeName.includes(manager.firstName) || employeeName.includes(manager.lastName));
+  return data.employees.find((manager) =>
+    employeeName.includes(manager.firstName) || employeeName.includes(manager.lastName));
 }
 console.log(getEmployeeByName());
 module.exports = getEmployeeByName;
--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,6 +1,9 @@
+const { TestScheduler } = require('jest');
 const handlerElephants = require('../src/handlerElephants');
 
 describe('Testes da função HandlerElephants', () => {
+    it(
     expect(handlerElephants('count')).toBe(4);
-    //expect(handlerElephants('names')).toEqual(["Ilana", "Orval", "Bea", "Jefferson"]);
+    expect(handlerElephants('names')).toEqual(["Ilana", "Orval", "Bea", "Jefferson"]);
+);
 });
