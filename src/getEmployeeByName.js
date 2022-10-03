47b285ff6edc1620fe67d03da973d35db6d9784f
Cauan <cauanbarros14@gmail.com>
2022-10-03 12:52:39 -0300

    requisito 3 arrumado

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,8 +1,9 @@
 const data = require('../data/zoo_data');
 
 function getEmployeeByName(...employeeName) {
-  if (employeeName.length === 0) return {};
-  return data.employees.find((manager) =>
+  if (employeeName.length === 0) {
+    return {};
+  } return data.employees.find((manager) =>
     employeeName.includes(manager.firstName) || employeeName.includes(manager.lastName));
 }
 console.log(getEmployeeByName());
