f14edd2b0cc1fa300f5efe01957afe3f6d35b6ba
Cauan <cauanbarros14@gmail.com>
2022-10-04 20:41:24 -0300

    cauan lindao

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,8 +1,8 @@
-const { species } = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const employee = data.employees.find((busca) => (id === busca.id));
+  const employee = employees.find((busca) => (id === busca.id));
   const idAnimal = employee.responsibleFor.find((specie) => specie[0]);
   return idAnimal;
 }
