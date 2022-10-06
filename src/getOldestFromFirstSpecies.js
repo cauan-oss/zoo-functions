dd84a50e81f1d44572886ee38d001c7919c780b5
Cauan <cauanbarros14@gmail.com>
2022-10-06 10:10:44 -0300

    requisito 10 concluido

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,10 +1,21 @@
-const { employees } = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
 function getOldestFromFirstSpecies(id) {
-  const employee = employees.find((busca) => (id === busca.id));
+  const employee = data.employees.find((busca) => id === busca.id);
+
   const idAnimal = employee.responsibleFor.find((specie) => specie[0]);
-  return idAnimal;
+  const test = species.filter((element) => idAnimal === element.id);
+  const ord = test[0].residents.sort((a, b) => b.age - a.age);
+  return Object.values(ord[0]);
 }
+console.log(getOldestFromFirstSpecies('9e7d4524-363c-416a-8759-8aa7e50c0992'));
 
 module.exports = getOldestFromFirstSpecies;
+
+/* const apple = idAnimal.filter((senior) => {
+    if (senior === employee.id) {
+      return species.residents.sort((velho) => velho[0]);
+    }
+  });
+   return apple; */
