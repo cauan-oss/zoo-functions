1b72c17636d839c55b8c12122952f7966e5920a3
cauan <cauanbarros14@gmail.com>
2023-01-16 19:24:12 -0300

    requisicao da api

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,10 +1,3 @@
 const data = require('../data/zoo_data');
 
-function getSpeciesByIds(...ids) {
-  const meuArray = [];
-
-  if (ids.length === 0) {
-    return meuArray;
-  } return data.species.filter((specie) => ids.includes(specie.id));
-}
-module.exports = getSpeciesByIds;
+console.log(data)
