37f7b6aa9166911f8ddbe087860a747bb2953ccf
cauan <cauanbarros14@gmail.com>
2023-01-16 20:54:37 -0300

    aprendendo o include

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,3 +1,19 @@
+const { species } = require('../data/zoo_data');
 const data = require('../data/zoo_data');
 
-console.log(data)
+/*const getSpeciesByIds = (...ids) => {
+ if (ids === 0) {
+   return []
+ } else {
+  return data.species.filter((specie) => specie.id )
+ }
+   
+   
+} */
+
+const array =  ['focus', 1, 2 ];
+console.log(array.includes(4))
+
+//const filterArray = data.species.filter((specie) => console.log(specie.id))
+
+//console.log(getSpeciesByIds('lionId'))
