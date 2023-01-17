7aa7cda5be6ecdc0a59ed6074c187d565468cf47
cauan <cauanbarros14@gmail.com>
2023-01-17 11:02:30 -0300

    fazendo a logica

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,10 +1,14 @@
 const data = require("../data/zoo_data");
 
-const getAnimalsOlderThan = (specie, age) => {
-    //data.species.map(() => );
+const getAnimalsOlderThan = (specie, idade) => {
+    const treino = data.species.find((animal) => specie === animal.name );
+    return treino.residents.map((reside) => reside.age )
 };
-const specieId = data.species.filter((specie) => specie.name);
 const specieAge = data.species.map((list) => list.residents )
 
 
-console.log(getAnimalsOlderThan());
+console.log(getAnimalsOlderThan('lions'));
+
+
+/*deve receber uma especie e uma idade como parametro
+ira fazer a comparacao se o animal e a idade passada no parametro tem a idade minima */
