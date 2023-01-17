489a1bded2b74539f6f8d4edf7b346e8b15eab6a
cauan <cauanbarros14@gmail.com>
2023-01-17 10:10:14 -0300

    iniciando terca

--- src/getAnimalsOlderThan.js
+++ src/getAnimalsOlderThan.js
@@ -1,5 +1,9 @@
 const data = require("../data/zoo_data");
 
-const getAnimalsOlderThan = (age) => {
+const getAnimalsOlderThan = (id, age) => {
+};
+const specieId = data.species.filter((specie) => specie.name);
+const specieAge = data.species.map((list) => list.residents.age )
 
-}
+
+console.log(specieAgeO);
